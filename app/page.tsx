9a3a2ec8d4fae14fdfce5e1c9b5f7f7f143677c5
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2024-03-18 23:13:53 -0400

    experiment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-row justify-evenly text-white items-center p-24">
+      <div className="p-20 bg-gradient-to-b from-[#c8d5bb] to [#47585c]">
+        Sabi nezu
+      </div>
+      <div className="p-20 bg-gradient-to-r from-[#80989b] to [#d4dcda]">
+        Usuku mo nezu
+      </div>
+      <div className="p-20 bg-gradient-to-b from-[#4d80e6] to [#e5e4e6]">
+        Nezuogusa
+      </div>
+      <div className="p-20 bg-gradient-to-b from-[#c8d5bb] to [#47585c]">
+        Yanagi nezumi
+      </div>
+    </main>
   );
 }
